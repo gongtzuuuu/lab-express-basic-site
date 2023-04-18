e3c8ea4e02c69d0679079b7ad6c23b542de502a2
Liz <claireliang2012@gmail.com>
2023-04-18 18:41:13 +0800

    Finish the bacis website

---
+++ app.js
@@ -0,0 +1,24 @@
+// Need to require Express first
+const express = require('express');
+const app = express();
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/home.html")
+})
+
+app.get("/about", (req, res) => {
+    res.sendFile(__dirname + "/views/about.html")
+})
+
+app.get("/works", (req, res) => {
+    res.sendFile(__dirname + "/views/works.html")
+})
+
+app.get("/gallery", (req, res) => {
+    res.sendFile(__dirname + "/views/gallery.html")
+})
+
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000!')
+});
